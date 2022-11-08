890e71994655c0c44cd58235c46b798045dd6cbe
Haelyne <114979106+Haelyne@users.noreply.github.com>
2022-11-07 22:04:09 -0800

    Add files via upload

---
+++ parts/meta.php
@@ -0,0 +1,6 @@
+	<meta charset="UTF-8">
+	<base href="http://haelyne.com/aau/ixd608/kim.haelyn/">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="css/storetheme.css">
